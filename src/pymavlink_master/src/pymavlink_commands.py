a95839cc97e4dc3c694198bbf55556421e040f4c
Ben <benaiahsaju@gmail.com>
2024-05-19 16:23:36 +0530

    Updated code structure and added launch file

--- src/pymavlink_master/src/pymavlink_commands.py
+++ src/pymavlink_master/src/pymavlink_commands.py
@@ -1,32 +1,54 @@
 #!/usr/bin/python3
-import rospy
-from pymavlink import mavutil
 from optparse import OptionParser
-from custom_msgs.msg import commands 
-from custom_msgs.msg import telemetry
 
-# 1 	Pitch
-# 2 	Roll
-# 3 	Throttle
-# 4 	Yaw
-# 5 	Forward
-# 6 	Lateral
+import rospy
+from custom_msgs.msg import commands, telemetry
+from pymavlink import mavutil
 
-class Basic:
+# Constants for channel mappings
+# 1   Pitch
+# 2   Roll
+# 3   Throttle
+# 4   Yaw
+# 5   Forward
+# 6   Lateral
 
-    def __init__(self) -> None:  
+
+class PixhawkMaster:
+    """
+    A class to interface with a Pixhawk via MAVLink, handle mode switching,
+    arming/disarming, and send telemetry data.
+    """
+
+    def __init__(self) -> None:
+        """
+        Initialize the Basic class with mode, port address, and set up the MAVLink connection.
+        """
         self.mode = options.auv_mode
         self.pixhawk_port = options.port_addr
-        self.arm_state= False            
+        self.arm_state = False
+
+        # Initialize MAVLink connection
         self.master = mavutil.mavlink_connection(self.pixhawk_port, baud=115200)
-        self.thruster_subs = rospy.Subscriber("/master/commands", commands, self.__callback__, queue_size=1)
-        self.telemetry_pub = rospy.Publisher('/master/telemetry', telemetry, queue_size=1)
-        self.channel_ary=[1500]*8
-        self.master.wait_heartbeat()
-        self.telem_msg = telemetry()
-        
-    def __callback__(self, msg):
-        self.channel_ary[0] = msg.pitch 
+
+        # ROS subscriber and publisher
+        self.thruster_subs = rospy.Subscriber(
+            "/master/commands", commands, self.__callback__, queue_size=1
+        )
+        self.telemetry_pub = rospy.Publisher(
+            "/master/telemetry", telemetry, queue_size=1
+        )
+
+        self.channel_ary = [1500] * 8  # Initialize channel values array
+        self.master.wait_heartbeat()  # Wait for the heartbeat from the Pixhawk
+        self.telem_msg = telemetry()  # Initialize telemetry message
+
+    def __callback(self, msg):
+        """
+        Callback function to handle incoming commands and update RC channel values.
+        Also handles arming/disarming and mode switching.
+        """
+        self.channel_ary[0] = msg.pitch
         self.channel_ary[1] = msg.roll
         self.channel_ary[2] = msg.thrust
         self.channel_ary[3] = msg.yaw
@@ -35,66 +57,100 @@ class Basic:
         self.channel_ary[6] = msg.servo1
         self.channel_ary[7] = msg.servo2
 
-        if (msg.arm == 1 and self.arm_state==False):
+        # Handle arming/disarming
+        if msg.arm == 1 and self.arm_state == False:
             self.arm()
             self.arm_state = True
-            
-        elif(msg.arm == 0 and self.arm_state==True):
+        elif msg.arm == 0 and self.arm_state == True:
             self.disarm()
             self.arm_state = False
 
-        
-        if (self.mode!=msg.mode):
-            if(self.arm_state == False):
-                self.mode=msg.mode
+        # Handle mode switching
+        if self.mode != msg.mode:
+            if self.arm_state == False:
+                self.mode = msg.mode
                 self.mode_switch()
             else:
-                print("Need to disarm in order to change mode")
+                rospy.logwarn("Disarm Pixhawk to change modes.")
 
     def arm(self):
+        """
+        Send an arm command to the Pixhawk.
+        """
         self.master.wait_heartbeat()
         self.master.mav.command_long_send(
-        self.master.target_system,
-        self.master.target_component,
-        mavutil.mavlink.MAV_CMD_COMPONENT_ARM_DISARM,
-        0,1, 0, 0, 0, 0, 0, 0)
-        print("arm command sent to pix")
-    
+            self.master.target_system,
+            self.master.target_component,
+            mavutil.mavlink.MAV_CMD_COMPONENT_ARM_DISARM,
+            0,
+            1,  # Arm
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+        )
+        rospy.loginfo("Arm command sent to Pixhawk")
+
     def disarm(self):
+        """
+        Send a disarm command to the Pixhawk.
+        """
         self.master.wait_heartbeat()
         self.master.mav.command_long_send(
-        self.master.target_system,
-        self.master.target_component,
-        mavutil.mavlink.MAV_CMD_COMPONENT_ARM_DISARM,
-        0,0, 0, 0, 0, 0, 0, 0)
-        print("sent disarm")
+            self.master.target_system,
+            self.master.target_component,
+            mavutil.mavlink.MAV_CMD_COMPONENT_ARM_DISARM,
+            0,
+            0,  # Disarm
+            0,
+            0,
+            0,
+            0,
+            0,
+            0,
+        )
+        rospy.loginfo("Disarm command sent to Pixhawk")
 
     def mode_switch(self):
+        """
+        Switch the Pixhawk mode.
+        """
         if self.mode not in self.master.mode_mapping():
-            print('Unknown mode : {}'.format(self.mode))
-            print('Try:', list(self.master.mode_mapping().keys()))
+            rospy.logerr(f"Unknown mode: {self.mode}")
+            rospy.loginfo(f"Try: {list(self.master.mode_mapping().keys())}")
             exit(1)
+
         mode_id = self.master.mode_mapping()[self.mode]
         self.master.mav.set_mode_send(
             self.master.target_system,
             mavutil.mavlink.MAV_MODE_FLAG_CUSTOM_MODE_ENABLED,
-            mode_id)
-        print("mode changed to:" ,(self.mode))
-        
+            mode_id,
+        )
+        rospy.loginfo(f"Mode changed to: {self.mode}")
+
     def set_rc_channel_pwm(self, id, pwm):
+        """
+        Set the PWM value for a specified RC channel.
+        """
         if id < 1:
-            print("Channel does not exist.")
+            rospy.logwarn("Channel does not exist.")
             return
-        
+
         if id < 9:
             rc_channel_values = [65535 for _ in range(8)]
             rc_channel_values[id - 1] = pwm
             self.master.mav.rc_channels_override_send(
-                self.master.target_system,                # target_system
-                self.master.target_component,             # target_component
-                *rc_channel_values)                  # RC channel list, in microseconds.
+                self.master.target_system,
+                self.master.target_component,
+                *rc_channel_values,
+            )  # RC channel list, in microseconds
 
     def actuate(self):
+        """
+        Send RC channel commands to the Pixhawk based on updated channel values.
+        """
         self.set_rc_channel_pwm(1, int(self.channel_ary[0]))
         self.set_rc_channel_pwm(2, int(self.channel_ary[1]))
         self.set_rc_channel_pwm(3, int(self.channel_ary[2]))
@@ -105,87 +161,107 @@ class Basic:
         self.set_rc_channel_pwm(8, int(self.channel_ary[7]))
 
     def request_message_interval(self, message_id: int, frequency_hz: float):
+        """
+        Request the interval at which a specified message should be sent.
+        """
         self.master.mav.command_long_send(
-            self.master.target_system, self.master.target_component,
-            mavutil.mavlink.MAV_CMD_SET_MESSAGE_INTERVAL, 0,
-            message_id, # The MAVLink message ID
-            1e6 / frequency_hz, # The interval between two messages in microseconds. Set to -1 to disable and 0 to request default rate. pure value is in microseconds
-            0, 0, 0, 0, # Unused parameters
-            0, # Target address of message stream (if message has target address fields). 0: Flight-stack default (recommended), 1: address of requestor, 2: broadcast.
+            self.master.target_system,
+            self.master.target_component,
+            mavutil.mavlink.MAV_CMD_SET_MESSAGE_INTERVAL,
+            0,
+            message_id,  # The MAVLink message ID
+            1e6 / frequency_hz,  # The interval between two messages in microseconds
+            0,
+            0,
+            0,
+            0,
+            0,
         )
-        response = self.master.recv_match(type='COMMAND_ACK', blocking=True)
-        if response and response.command == mavutil.mavlink.MAV_CMD_SET_MESSAGE_INTERVAL and response.result == mavutil.mavlink.MAV_RESULT_ACCEPTED:
-            print("Command accepted")
+        response = self.master.recv_match(type="COMMAND_ACK", blocking=True)
+        if (
+            response
+            and response.command == mavutil.mavlink.MAV_CMD_SET_MESSAGE_INTERVAL
+            and response.result == mavutil.mavlink.MAV_RESULT_ACCEPTED
+        ):
+            rospy.loginfo("Command Accepted")
         else:
-            print("Command failed")
-                
-
-
-
+            rospy.logerr("Command Failed")
 
     def telem_publish_func(self, imu_msg, attitude_msg, vfr_hud_msg, depth_msg):
-        self.telem_msg.timestamp=imu_msg.time_boot_ms
-        self.telem_msg.internal_pressure=vfr_hud_msg.alt
-        self.telem_msg.external_pressure=depth_msg.press_abs
-        self.telem_msg.heading=vfr_hud_msg.heading
-        self.telem_msg.imu_gyro_x=imu_msg.xgyro
-        self.telem_msg.imu_gyro_y=imu_msg.ygyro
-        self.telem_msg.imu_gyro_z=imu_msg.zgyro
-        self.telem_msg.imu_gyro_compass_x=imu_msg.xmag
-        self.telem_msg.imu_gyro_compass_y=imu_msg.ymag
-        #self.telem_msg.imu_gyro_compass_z=vfr_hud_msg.yaw
-        self.telem_msg.q1=attitude_msg.q1
-        self.telem_msg.q2=attitude_msg.q2
-        self.telem_msg.q3=attitude_msg.q3
-        self.telem_msg.q4=attitude_msg.q4
-        self.telem_msg.rollspeed=attitude_msg.rollspeed
-        self.telem_msg.pitchspeed=attitude_msg.pitchspeed
-        self.telem_msg.yawspeed=attitude_msg.yawspeed
+        """
+        Publish telemetry data based on received MAVLink messages.
+        """
+        self.telem_msg.timestamp = imu_msg.time_boot_ms
+        self.telem_msg.internal_pressure = vfr_hud_msg.alt
+        self.telem_msg.external_pressure = depth_msg.press_abs
+        self.telem_msg.heading = vfr_hud_msg.heading
+        self.telem_msg.imu_gyro_x = imu_msg.xgyro
+        self.telem_msg.imu_gyro_y = imu_msg.ygyro
+        self.telem_msg.imu_gyro_z = imu_msg.zgyro
+        self.telem_msg.imu_gyro_compass_x = imu_msg.xmag
+        self.telem_msg.imu_gyro_compass_y = imu_msg.ymag
+        self.telem_msg.q1 = attitude_msg.q1
+        self.telem_msg.q2 = attitude_msg.q2
+        self.telem_msg.q3 = attitude_msg.q3
+        self.telem_msg.q4 = attitude_msg.q4
+        self.telem_msg.rollspeed = attitude_msg.rollspeed
+        self.telem_msg.pitchspeed = attitude_msg.pitchspeed
+        self.telem_msg.yawspeed = attitude_msg.yawspeed
+
         self.telemetry_pub.publish(self.telem_msg)
 
 
-
-
-
-
-
-        
-
-
-
-
 if __name__ == "__main__":
-    rospy.init_node('pymav_master', anonymous=True)
-    
-    parser = OptionParser(description='description for prog')
-    parser.add_option("-p", "--port", dest="port_addr",default="/dev/Pixhawk",help="Pass Pixhawk Port Address", metavar="VAR")
-    parser.add_option("-m", "--mode", dest="auv_mode", default='STABILIZE',help="Pass Pixhawk Mode", metavar="VAR")
-    (options, args) = parser.parse_args()
-    obj = Basic()
+    # Initialize ROS node
+    rospy.init_node("pymav_master", anonymous=True)
 
+    # Command line options
+    parser = OptionParser(description="description for prog")
+    parser.add_option(
+        "-p",
+        "--port",
+        dest="port_addr",
+        default="/dev/Pixhawk",
+        help="Pass Pixhawk Port Address",
+        metavar="VAR",
+    )
+    parser.add_option(
+        "-m",
+        "--mode",
+        dest="auv_mode",
+        default="STABILIZE",
+        help="Pass Pixhawk Mode",
+        metavar="VAR",
+    )
+    (options, args) = parser.parse_args()
+
+    # Instantiate class
+    obj = PixhawkMaster()
+
+    # Request message intervals
     obj.request_message_interval(mavutil.mavlink.MAVLINK_MSG_ID_HEARTBEAT, 2000)
-    obj.request_message_interval(mavutil.mavlink.MAVLINK_MSG_ID_ATTITUDE_QUATERNION, 100)
+    obj.request_message_interval(
+        mavutil.mavlink.MAVLINK_MSG_ID_ATTITUDE_QUATERNION, 100
+    )
     obj.request_message_interval(mavutil.mavlink.MAVLINK_MSG_ID_AHRS2, 100)
     obj.request_message_interval(mavutil.mavlink.MAVLINK_MSG_ID_SCALED_PRESSURE2, 100)
-    obj.request_message_interval(mavutil.mavlink.MAVLINK_MSG_ID_VFR_HUD,100)
-    obj.request_message_interval(mavutil.mavlink.MAVLINK_MSG_ID_SCALED_IMU2,100)
-    #obj.master.mav.command_long_send(obj.master.target_system, obj.master.target_component, mavutil.mavlink.MAV_FRAME_LOCAL_NED, 10, 0, -10, 0, 0, 0, 0, 0)
-    while not rospy.is_shutdown():
-        #print(obj.master.attitude)
-        obj.actuate()
-        try:  
-            msg_imu = obj.master.recv_match(type='SCALED_IMU2',blocking=True)
-            msg_attitude = obj.master.recv_match(type='ATTITUDE_QUATERNION',blocking=True)
-            msg_vfr_hud = obj.master.recv_match(type='VFR_HUD',blocking=True)
-            msg_depth = obj.master.recv_match(type='SCALED_PRESSURE2',blocking=True)
-            #msg_d = obj.master.recv_match(type='AHRS3',blocking=True)
-            #print(msg_d)
-        except:
-            pass
-        obj.telem_publish_func(msg_imu,msg_attitude,msg_vfr_hud,msg_depth)
-            #print('No message recieved')
-        #obj.telem_publish_func(msg_imu,msg_attitude,msg_vfr_hud,msg_depth)
+    obj.request_message_interval(mavutil.mavlink.MAVLINK_MSG_ID_VFR_HUD, 100)
+    obj.request_message_interval(mavutil.mavlink.MAVLINK_MSG_ID_SCALED_IMU2, 100)
 
-        
-        
-    # rospy.spin()
+    # Main loop
+    while not rospy.is_shutdown():
+        obj.actuate()
+        try:
+            # Receive MAVLink messages
+            msg_imu = obj.master.recv_match(type="SCALED_IMU2", blocking=True)
+            msg_attitude = obj.master.recv_match(
+                type="ATTITUDE_QUATERNION", blocking=True
+            )
+            msg_vfr_hud = obj.master.recv_match(type="VFR_HUD", blocking=True)
+            msg_depth = obj.master.recv_match(type="SCALED_PRESSURE2", blocking=True)
+        except Exception as e:
+            rospy.logwarn(f"Error receiving message: {e}")
+            continue
+
+        # Publish telemetry data
+        obj.telem_publish_func(msg_imu, msg_attitude, msg_vfr_hud, msg_depth)
--- src/pymavlink_master/src/yaw_publisher.py
+++ src/pymavlink_master/src/yaw_publisher.py
@@ -6,13 +6,25 @@ from std_msgs.msg import Int32
 
 
 def main():
+    """
+    Initializes the ROS node, sets up the serial connection, and publishes yaw data
+    from an Arduino to a ROS topic.
+    """
     rospy.init_node("arduino_yaw_publisher", anonymous=True)
     pub = rospy.Publisher("mira/heading", Int32, queue_size=10)
     rate = rospy.Rate(10)
 
+    # Parameters for serial port and baud rate
     serial_port = rospy.get_param("~serial_port", "/dev/Mega")
     serial_baud = rospy.get_param("~serial_baud", 115200)
-    ser = serial.Serial(serial_port, serial_baud)
+
+    # Establish serial connection
+    try:
+        ser = serial.Serial(serial_port, serial_baud)
+        rospy.loginfo(f"Connected to {serial_port} at {serial_baud} baud.")
+    except serial.SerialException as e:
+        rospy.logerr(f"Failed to connect to {serial_port} at {serial_baud} baud: {e}")
+        return
 
     while not rospy.is_shutdown():
         if ser.in_waiting > 0:
@@ -20,12 +32,14 @@ def main():
                 line = ser.readline().decode("utf-8").strip()
                 yaw_value = int(float(line))
                 pub.publish(yaw_value)
+                rospy.loginfo(f"Published Yaw value: {yaw_value}")
             except ValueError as e:
                 rospy.logwarn(f"ValueError: {e} with line '{line}'")
+        rate.sleep()
 
 
 if __name__ == "__main__":
     try:
         main()
     except rospy.ROSInterruptException:
-        pass
+        rospy.loginfo("Yaw Publisher Node Terminated.")
